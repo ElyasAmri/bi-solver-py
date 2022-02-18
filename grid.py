fc06dfb987b130ff7a44aba7c0c76736cd1fe979
ElyasAmri <elyas.amri@gmail.com>
2022-02-18 12:08:25 +0300

    Working step 1

--- grid.py
+++ grid.py
@@ -1,4 +1,4 @@
-from bits import listformat
+from bits import listformat, binary, binaryx
 
 
 class Grid:
@@ -12,7 +12,22 @@ class Grid:
         print()
         for i in range(0, self.h):
             l = [self.data[(i, j)] for j in range(0, self.w)]
-            print(f'i         {listformat(l)}')
+            print(f'{i}         {listformat(l)}')
+        print()
+
+    def print_bin(self):
+        print(f'{binary(0)}         {listformat([binary(j) for j in range(0, self.w)])}')
+        print()
+        for i in range(0, self.h):
+            l = [binary(self.data[(i, j)]) for j in range(0, self.w)]
+            print(f'{binary(i)}         {listformat(l)}')
+        print()
+
+    def print_bin_digit(self, digit):
+        for i in range(0, self.h):
+            l = [binaryx(self.data[(i, j)], 8, -digit) for j in range(0, self.w)]
+            print(f'{binary(i)}         {listformat(l)}')
+        print()
 
     def sum(self):
         return sum(self.data.values())
--- solution.py
+++ solution.py
@@ -3,42 +3,50 @@ from grid import Grid
 
 
 def table_sum(w, h):
-    mx = floor(log2(max(w, h)))
+    g = Grid(w, h)
+    # g.print()
+    # g.print_bin()
+
+    mx = ceil(log2(max(w, h)))
     res = 0
     for i in range(0, mx):
+        g.print_bin_digit(i+1)
+
         d = 2 ** i
         dvi = floor(w / d)
         dvj = floor(h / d)
         ri = w % d
         rj = h % d
 
-        im = dvi % 2 == 0
-        jm = dvi % 2 == 0
-
-        if im or jm:
+        if dvi % 2 == 0 or dvj % 2 == 0:
             squares = dvi * dvj / 2
         else:
-            squares = (dvi * dvj + 1) / 2
+            squares = (dvi * dvj - 1) / 2
 
-        if im:
-            remainders = dvi * rj / 2
-        elif jm:
-            remainders = dvj * ri / 2
+        if dvi % 2 == 1 and dvj % 2 == 1:
+            remainders = ((dvi + 1) * rj + (dvj + 1) * ri) / 2 * d
         else:
-            remainders = ((dvi - 1) * rj + (dvj - 1) * ri) / 2
+            remainders = ((dvi - dvi % 2) * rj + (dvj - dvj % 2) * ri) / 2 * d
 
-        res += (squares * d + remainders) * (d ** 2)
+        if not dvi % 2 == dvj % 2:
+            remainders += ri * rj
 
-    return int(res)
+        c = squares * d * d + remainders
+        val = c * d
+        print(f'd: {d}, count: {c}, dvi: {dvi}, ri: {ri}, dvj: {dvj}, rj: {rj} val:{val}')
+        print(f'      squares: {squares}, remainders: {remainders}')
+        print()
+        res += val
+
+    print(g.sum())
+    print(res)
 
 
 def main():
     w = 16
     h = 16
-    g = Grid(w, h)
 
-    print(g.sum())
-    print(table_sum(w, h))
+    table_sum(w, h)
 
 
 if __name__ == '__main__':
