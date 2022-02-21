5ed8518c6382f077c9c6ec6bb7f8d228508036b9
ElyasAmri <elyas.amri@gmail.com>
2022-02-21 21:31:14 +0300

    Double print frames data + rename bits to funcs

---
+++ funcs.py
@@ -0,0 +1,38 @@
+def binary(n, zf=8):
+    return bin(n).zfill(zf).replace('b', '')
+
+
+def binary_digit(n, zf=8, i=-1):
+    return int(binary(n, zf)[i])
+
+
+def list_format(l: list):
+    return list_format_str(str(l))
+
+
+def list_format_str(l: str):
+    return l.replace('[', '').replace(']', '').replace("'", '').replace(',', '')
+
+
+def combine_lists(tab, *lists: list):
+    return tab.join([str(i).replace('[', '').replace(']', '').replace(',', '') for i in lists])
+
+
+def list_add_list(l1, l2):
+    assert len(l1) == len(l2)
+    return [l1[i] + l2[i] for i in range(0, len(l1))]
+
+
+def list_sub_list(l1, l2):
+    assert len(l1) == len(l2)
+    return [l1[i] - l2[i] for i in range(0, len(l1))]
+
+
+def list_sub_list_bitwise(l1, l2):
+    assert len(l1) == len(l2)
+    return [0 if l1[i] == l2[i] == 1 else 1 for i in range(0, len(l1))]
+
+def list_sub_list_bitwise_rev(l1, l2):
+    assert len(l1) == len(l2)
+    return [max(l1[i] - l2[i], 0) for i in range(0, len(l1))]
+
--- grid.py
+++ grid.py
@@ -1,6 +1,6 @@
 from math import *
 
-from bits import *
+from funcs import *
 
 
 class Grid:
@@ -8,6 +8,7 @@ class Grid:
         self.w = w
         self.h = h
         self.L = L
+        self.mx = ceil(log2(max(w, h))) + 1
 
         if not L == 0:
             self.data = {(i, j): max((i ^ j) - L, 0) for i in range(0, h) for j in range(0, w)}
@@ -16,33 +17,50 @@ class Grid:
             self.data = {(i, j): i ^ j for i in range(0, h) for j in range(0, w)}
 
     def print(self):
-        print(f'0         {listformat([j for j in range(0, self.w)])}\n')
+        print(f'0         {list_format([j for j in range(0, self.w)])}\n')
         for i in range(0, self.h):
             l = [self.data[(i, j)] for j in range(0, self.w)]
-            print(f'{i}         {listformat(l)}')
+            print(f'{i}         {list_format(l)}')
         print()
 
     def print_bin(self):
-        print(f'{binary(0)}         {listformat([binary(j) for j in range(0, self.w)])}\n')
+        print(f'{binary(0)}         {list_format([binary(j) for j in range(0, self.w)])}\n')
         for i in range(0, self.h):
             l = [binary(self.data[(i, j)]) for j in range(0, self.w)]
-            print(f'{binary(i)}         {listformat(l)}')
+            print(f'{binary(i)}         {list_format(l)}')
         print()
 
     def print_bin_digit(self, digit):
         for i in range(0, self.h):
             l = [binary_digit(self.data[(i, j)], 8, -digit) for j in range(0, self.w)]
-            print(f'{binary(i)}         {listformat(l)}')
+            print(f'{binary(i)}         {list_format(l)}')
         print()
 
     def print_frames(self):
-        mx = ceil(log2(max(self.w, self.h)))
-        for d in range(1, mx + 1):
+        subtract_digits = [int(i) for i in binary(self.L, 8)]
+        last_s = None
+        for d in range(1, self.mx):
+            new_s = False
             for i in range(0, self.h):
-                print(listformatstr(
-                    str([binary_digit(self.raw.data[(i, j)], 8, -d) for j in range(0, self.w)]) + '    ' + str(
-                        [binary_digit(self.data[(i, j)], 8, -d) for j in range(0, self.w)])))
-            print('\n----------\n')
+                lp = [binary_digit(self.raw.data[(i, j)], self.mx, -d) for j in range(0, self.w)]
+                la = [binary_digit(self.data[(i, j)], self.mx, -d) for j in range(0, self.w)]
+
+                if d in subtract_digits:
+                    if not new_s:
+                        print('\n--------------------\n')
+                        last_s = [[0 for _ in range(0, self.w)] for _ in range(0, self.h)]
+                        new_s = True
+
+                    lpr = [0 if i == 1 else 1 for i in lp]
+                    last_s[i] = list_add_list(last_s[i], lpr)
+                    ld = [0 for _ in lp]
+                    print(combine_lists('    ', lp, la, ld, lpr))
+                else:
+                    new_s = False
+                    lp_s = list_sub_list_bitwise(lp, last_s[i])
+                    last_s[i] = list_sub_list_bitwise_rev(last_s[i], lp)
+                    print(combine_lists('    ', lp, la, lp_s, last_s[i]))
+            print()
 
     def sum(self):
         return sum(self.data.values())
